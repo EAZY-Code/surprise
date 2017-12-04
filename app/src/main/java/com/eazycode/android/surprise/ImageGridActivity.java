28cdf49670d1a986abd5057de4197dd18051de9b
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2017-12-04 08:17:19 +0000

    Original commit

---
+++ app/src/main/java/com/eazycode/android/surprise/ImageGridActivity.java
@@ -0,0 +1,13 @@
+package com.eazycode.android.surprise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_grid);
+    }
+}
